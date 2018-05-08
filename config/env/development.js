3e48414b817c30488f368e56f0655a1d19fda167
iMac <imac@iMacs-iMac.local>
2018-05-09 00:17:22 +0500

    this is all code

---
+++ config/env/development.js
@@ -0,0 +1,12 @@
+'use strict';
+const config = {
+	db: {
+		database: 'blog-dev',
+		username: 'root',
+		password: '',
+		host: 'localhost',
+		port: 3306,
+        dialect: 'mysql'
+	}
+};
+export default config;
